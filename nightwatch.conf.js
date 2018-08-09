95d4f97acc60ff9377061fb60e49ddff3ce1fd02
Vikrant Mulani <Vikrant_mulani@yahoo.co.in>
2018-08-09 23:55:09 +1200

    Initial commit

---
+++ nightwatch.conf.js
@@ -0,0 +1,7 @@
+/**
+ * Created by vikrant.mulani on 8/07/18.
+ */
+module.exports = (function(settings) {
+    settings.test_workers = false;
+    return settings;
+})(require('./nightwatch.json'));
